5da6787d85f32de36a5b284eb657e34efa47a7b7
TvanMeer <tvanmeer123@gmail.com>
2021-09-17 02:13:28 +0200

    Add symbol model

---
+++ scalpr/database/symbol.py
@@ -0,0 +1,16 @@
+# pylint: disable=no-name-in-module
+
+from typing import Dict
+
+from pydantic import BaseModel
+from pydantic.types import constr
+
+from ..core.constants import Interval
+from .window import Window
+
+
+class Symbol(BaseModel):
+    """Holds all data related to a symbol, such as `BTCUSDT`."""
+
+    name:    constr(strip_whitespace=True, to_lower=True, min_length=3, max_length=6)
+    windows: Dict[Interval, Window] = dict()

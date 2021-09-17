4385f4d3fb9b79cce58c07b51b901461e80a0eda
TvanMeer <tvanmeer123@gmail.com>
2021-09-17 05:09:46 +0200

    Move historical candlepipe class to candlepipe.py

--- scalpr/pipeline/candle_pipe.py
+++ scalpr/pipeline/candle_pipe.py
@@ -1,4 +1,4 @@
-from typing import Dict
+from typing import Dict, List
 
 from ..core.constants import InTimeFrame
 from ..database.candle import Candle
@@ -31,3 +31,31 @@ class CandlePipe(Pipe):
     
     def insert_in_next_timeframe(self, parsed: Candle, window: Window) -> Window:
         pass
+
+
+class HistoricalCandlePipe(Pipe):
+
+        
+    def which_timeframe(self, payload: List, window: Window) -> InTimeFrame:
+        pass
+
+
+    def parse(self, payload: List) -> Candle:
+        pass
+
+ 
+    def insert_in_first_timeframe(self, parsed: Candle, window: Window) -> Window:
+        pass
+
+
+    def insert_in_previous_timeframe(self, parsed: Candle, window: Window) -> Window:
+        pass
+
+
+    def insert_in_current_timeframe(self, parsed: Candle, window: Window) -> Window:
+        pass
+
+    
+    def insert_in_next_timeframe(self, parsed: Candle, window: Window) -> Window:
+        pass
+

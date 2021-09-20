a9f38e7246c94a045e0fa3a986758a809484ef64
TvanMeer <tvanmeer123@gmail.com>
2021-09-20 04:05:43 +0200

    Refactor depth model

--- scalpr/database/depth.py
+++ scalpr/database/depth.py
@@ -1,62 +1,30 @@
 # pylint: disable=no-name-in-module
 
-from typing import Tuple
 from datetime import datetime
+from typing import Deque, Tuple
 
 from pydantic import BaseModel
-from pydantic.types import condecimal
 
 
-class Depth5(BaseModel):
+class Bid(BaseModel):
+    price:    float
+    quantity: float
+
+
+class Ask(BaseModel):
+    price:    float
+    quantity: float
+
+class Depth(BaseModel):
     """Depthchart, showing the best bids and asks in the orderbook.
-    Bid1 is the best bid, bid2 the second best.
-    Ask1 is the best ask, ask2 the second best etcetera.
+    orders[0] is the best bid and ask, orders[1] the second best etc.
+    The orders are a snapshot of the depthcache at close time of the timeframe.
 
-    Every entry is a tuple of <price, quantity>
+    The value Options._depthcache_size represents the number of orders saved
+    in Depth.orders.
+    E.g. if Options._depthcache_size is equal to 5, the top 5 best bids and asks
+    will be saved in Depth.orders.
     """
 
-    last_update_time: datetime    
-    bid1:  Tuple[condecimal(decimal_places=8), condecimal(decimal_places=8)]
-    ask1:  Tuple[condecimal(decimal_places=8), condecimal(decimal_places=8)]
-    bid2:  Tuple[condecimal(decimal_places=8), condecimal(decimal_places=8)]
-    ask2:  Tuple[condecimal(decimal_places=8), condecimal(decimal_places=8)]
-    bid3:  Tuple[condecimal(decimal_places=8), condecimal(decimal_places=8)]
-    ask3:  Tuple[condecimal(decimal_places=8), condecimal(decimal_places=8)]
-    bid4:  Tuple[condecimal(decimal_places=8), condecimal(decimal_places=8)]
-    ask4:  Tuple[condecimal(decimal_places=8), condecimal(decimal_places=8)]
-    bid5:  Tuple[condecimal(decimal_places=8), condecimal(decimal_places=8)]
-    ask5:  Tuple[condecimal(decimal_places=8), condecimal(decimal_places=8)]
-
-class Depth10(Depth5):
-    bid6:  Tuple[condecimal(decimal_places=8), condecimal(decimal_places=8)]
-    ask6:  Tuple[condecimal(decimal_places=8), condecimal(decimal_places=8)]
-    bid7:  Tuple[condecimal(decimal_places=8), condecimal(decimal_places=8)]
-    ask7:  Tuple[condecimal(decimal_places=8), condecimal(decimal_places=8)]
-    bid8:  Tuple[condecimal(decimal_places=8), condecimal(decimal_places=8)]
-    ask8:  Tuple[condecimal(decimal_places=8), condecimal(decimal_places=8)]
-    bid9:  Tuple[condecimal(decimal_places=8), condecimal(decimal_places=8)]
-    ask9:  Tuple[condecimal(decimal_places=8), condecimal(decimal_places=8)]
-    bid10: Tuple[condecimal(decimal_places=8), condecimal(decimal_places=8)]
-    ask10: Tuple[condecimal(decimal_places=8), condecimal(decimal_places=8)]
-
-class Depth20(Depth10):
-    bid11: Tuple[condecimal(decimal_places=8), condecimal(decimal_places=8)]
-    ask11: Tuple[condecimal(decimal_places=8), condecimal(decimal_places=8)]
-    bid12: Tuple[condecimal(decimal_places=8), condecimal(decimal_places=8)]
-    ask12: Tuple[condecimal(decimal_places=8), condecimal(decimal_places=8)]
-    bid13: Tuple[condecimal(decimal_places=8), condecimal(decimal_places=8)]
-    ask13: Tuple[condecimal(decimal_places=8), condecimal(decimal_places=8)]
-    bid14: Tuple[condecimal(decimal_places=8), condecimal(decimal_places=8)]
-    ask14: Tuple[condecimal(decimal_places=8), condecimal(decimal_places=8)]
-    bid15: Tuple[condecimal(decimal_places=8), condecimal(decimal_places=8)]
-    ask15: Tuple[condecimal(decimal_places=8), condecimal(decimal_places=8)]
-    bid16: Tuple[condecimal(decimal_places=8), condecimal(decimal_places=8)]
-    ask16: Tuple[condecimal(decimal_places=8), condecimal(decimal_places=8)]
-    bid17: Tuple[condecimal(decimal_places=8), condecimal(decimal_places=8)]
-    ask17: Tuple[condecimal(decimal_places=8), condecimal(decimal_places=8)]
-    bid18: Tuple[condecimal(decimal_places=8), condecimal(decimal_places=8)]
-    ask18: Tuple[condecimal(decimal_places=8), condecimal(decimal_places=8)]
-    bid19: Tuple[condecimal(decimal_places=8), condecimal(decimal_places=8)]
-    ask19: Tuple[condecimal(decimal_places=8), condecimal(decimal_places=8)]
-    bid20: Tuple[condecimal(decimal_places=8), condecimal(decimal_places=8)]
-    ask20: Tuple[condecimal(decimal_places=8), condecimal(decimal_places=8)]
+    orders:            Deque[Tuple[Bid, Ask]]
+    last_update_time:  datetime

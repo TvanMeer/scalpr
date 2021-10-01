d42d5513069dd231c94b79d0afcaaa70fb90c8aa
TvanMeer <tvanmeer123@gmail.com>
2021-10-01 04:57:08 +0200

    Update manager and implement shutdown func

--- scalpr/core/manager.py
+++ scalpr/core/manager.py
@@ -1,8 +1,10 @@
 import asyncio
+from typing import List
 
 from binance import AsyncClient, BinanceSocketManager
 
 from ..database.database import DataBase
+from ..loops.factory import CoroFactory, Shared
 from ..options import Options
 from .constants import Mode
 from .prepare_db import prepare
@@ -21,11 +23,13 @@ class Manager:
     async def start(self, options: Options):
         self.client = await self.create_client(options)
         self.socket_manager = BinanceSocketManager(self.client)
-        self.db = self.create_database(options, self.client)
-        #TODO
+        self.db = await self.create_database(options, self.client)
+        self.shared = Shared()
+        self.tasks = await self.create_tasks(self.db, self.shared, self.client, self.socket_manager)
+        _ = await asyncio.gather(*self.tasks)
 
 
-    async def create_client(self, options: Options):
+    async def create_client(self, options: Options) -> AsyncClient:
         return await AsyncClient.create(
             options.key, 
             options.secret, 
@@ -33,11 +37,20 @@ class Manager:
         )
 
 
-    def create_database(self, options: Options, client: AsyncClient) -> DataBase:
+    async def create_database(self, options: Options, client: AsyncClient) -> DataBase:
         db = DataBase(options)
-        db = prepare(options, db, client)
+        db = await prepare(options, db, client)
         return db
 
 
+    async def create_tasks(self, db: DataBase, shared_vars: Shared, client: AsyncClient, manager: BinanceSocketManager) -> List[asyncio.Task]:
+        factory = CoroFactory(db, shared_vars, client, manager)
+        return factory.get_coros()
+
+
     async def shutdown(self):
+        print("Shutting down Scalpr in four seconds...")
+        self.shared.shutdown = True
+        await asyncio.sleep(4)
         await self.client.close_connection()
+        print("Scalpr closed gracefully.")

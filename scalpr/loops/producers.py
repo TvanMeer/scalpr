de9f6fbe752d5419f2aeea59ad4df9cfab3ee7ec
TvanMeer <tvanmeer123@gmail.com>
2021-10-01 04:16:52 +0200

    Change params and refactor historical candle producer

--- scalpr/loops/producers.py
+++ scalpr/loops/producers.py
@@ -1,13 +1,17 @@
-from typing import Set
+
+import asyncio
 
 from binance import AsyncClient, BinanceSocketManager
 
-from ..core.constants import ContentType, Interval
+from ..core.constants import ContentType
+from ..database.database import DataBase
 from ..pipeline.pipe import Message, Pipe
 from .shared_vars import Shared
 
 
 async def candle_producer(symbol: str, shared: Shared, manager: BinanceSocketManager):
+    """Spawned one for every selected symbol."""
+
     socket = manager.kline_socket(symbol=symbol)
     async with socket as candle_socket:
         while not shared.shutdown:
@@ -21,29 +25,42 @@ async def candle_producer(symbol: str, shared: Shared, manager: BinanceSocketMan
             await shared.queue.put(message)
 
 
-async def historical_candle_producer(symbol: str, shared: Shared, intervals: Set[Interval], windowsize: int, client: AsyncClient):
 
-    def to_timestring(interval: str, windowsize: int) -> str:
-        amount = int(interval[:-1]) * windowsize
-        time_frame = interval[-1]
-        time_frames = {
-            "m": f"{amount} minutes ago UTC",
-            "h": f"{amount} hours ago UTC",
-            "d": f"{amount} days ago UTC",
-            "w": f"{amount} weeks ago UTC"
-        }
-        return time_frames[time_frame]
+def to_timestring(interval: str, window_length: int) -> str:
+    """Helper func for downloading history."""
 
-    for iv in intervals:
-        time_string = to_timestring(iv.value, windowsize)
-        candles= await client.get_historical_klines(symbol, iv.value, time_string,limit=windowsize)
-        for c in candles:
-            message = Message(
-                time         =Pipe.to_datetime(time=c[6]),
-                symbol       =symbol,
-                content_type =ContentType.CANDLE_HISTORY,
-                payload      =c,
-                interval     =iv
-            )
-            await shared.queue.put(message)
-        if shared.shutdown: break
+    amount = int(interval[:-1]) * window_length
+    time_frame = interval[-1]
+    time_frames = {
+        "m": f"{amount} minutes ago UTC",
+        "h": f"{amount} hours ago UTC",
+        "d": f"{amount} days ago UTC",
+        "w": f"{amount} weeks ago UTC"
+    }
+    return time_frames[time_frame]
+
+
+
+async def historical_candle_producer(shared: Shared, client: AsyncClient, db: DataBase):
+    """Spawned one only for all selected symbols."""
+
+    window_length = db.options.window_length
+    intervals = db.options.window_intervals
+    symbols = db.selected_symbols
+
+    for s in symbols:
+        for iv in intervals:
+            time_string = to_timestring(iv.value, window_length)
+            candles= await client.get_historical_klines(s, iv.value, time_string, limit=window_length)
+            for c in candles:
+                message = Message(
+                    time         =Pipe.to_datetime(time=c[6]),
+                    symbol       =s,
+                    content_type =ContentType.CANDLE_HISTORY,
+                    payload      =c,
+                    interval     =iv
+                )
+                await shared.queue.put(message)
+            db.symbols[s].windows[iv]._history_downloaded = True
+            await asyncio.sleep(4)
+            if shared.shutdown: break

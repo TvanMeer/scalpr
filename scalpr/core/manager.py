67bf487276199eea96b600d77cf02a4a79154d7b
TvanMeer <tvanmeer123@gmail.com>
2021-10-08 07:59:48 +0200

    Rewrite manager logic to work with new loops interface

--- scalpr/core/manager.py
+++ scalpr/core/manager.py
@@ -1,13 +1,15 @@
 import asyncio
+from queue import Queue
 from typing import List
 
 from binance import AsyncClient, BinanceSocketManager
 
-from ..database.database import DataBase
-from ..loops.factory import CoroFactory, Shared
+from ..database.factory import DatabaseFactory
+from ..loops.director import (Director, HistoryDirector, PaperDirector,
+                              StreamDirector, TestDirector, TradeDirector)
+from ..loops.state import SharedState
 from ..options import Options
 from .constants import Mode
-from .prepare_db import prepare
 
 
 class Manager:
@@ -15,18 +17,23 @@ class Manager:
     Ensures time synchronization between all producers.
     """
 
-    def __init__(self, options: Options):
+    def start(self, options: Options, userinput_queue: Queue):
         loop = asyncio.get_event_loop()
-        loop.run_until_complete(self.start(options))
+        loop.run_until_complete(self.start_async(options, userinput_queue))
 
 
-    async def start(self, options: Options):
-        self.client = await self.create_client(options)
-        self.socket_manager = BinanceSocketManager(self.client)
-        self.db = await self.create_database(options, self.client)
-        self.shared = Shared()
-        self.tasks = await self.create_tasks(self.db, self.shared, self.client, self.socket_manager)
-        _ = await asyncio.gather(*self.tasks)
+
+    async def start_async(self, options: Options, userinput_queue: Queue):
+        client = await self.create_client(options)
+        socket_manager = BinanceSocketManager(client)
+        db_factory = DatabaseFactory()
+        database = await db_factory.create_database(options, client)
+        state = SharedState(database, userinput_queue)
+        director = self.create_loops_director(options)
+        loops = self.create_loops(state, director, client, socket_manager)
+        _ = await asyncio.gather(*loops)
+        await client.close_connection()
+
 
 
     async def create_client(self, options: Options) -> AsyncClient:
@@ -37,20 +44,22 @@ class Manager:
         )
 
 
-    async def create_database(self, options: Options, client: AsyncClient) -> DataBase:
-        db = DataBase(options)
-        db = await prepare(options, db, client)
-        return db
+
+    def create_loops_director(self, options: Options) -> Director:
+        directors = {
+            Mode.TEST:    TestDirector,
+            Mode.HISTORY: HistoryDirector,
+            Mode.STREAM:  StreamDirector,
+            Mode.PAPER:   PaperDirector,
+            Mode.TRADE:   TradeDirector
+        }
+        return directors[options.mode]
 
 
-    async def create_tasks(self, db: DataBase, shared_vars: Shared, client: AsyncClient, manager: BinanceSocketManager) -> List[asyncio.Task]:
-        factory = CoroFactory(db, shared_vars, client, manager)
-        return factory.get_coros()
 
-
-    async def shutdown(self):
-        print("Shutting down Scalpr in four seconds...")
-        self.shared.shutdown = True
-        await asyncio.sleep(4)
-        await self.client.close_connection()
-        print("Scalpr closed gracefully.")
+    def create_loops(self, state: SharedState, director: Director, client: AsyncClient, manager: BinanceSocketManager) -> List[asyncio.Task]:
+        if director is TestDirector:
+            return TestDirector.construct(state)
+        if director is HistoryDirector:
+            return HistoryDirector.construct(state, client)
+        return director.construct(state, client, manager)
